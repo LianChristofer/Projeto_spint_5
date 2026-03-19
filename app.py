89c4e741a65037bed634f7deef8b6dfdd7ffd64b
LianChristofer <liancsbertuola@gmail.com>
2026-03-19 16:46:06 +0000

    mod4

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ car_data = pd.read_csv('vehicles.csv')
 
 st.header("Analise Exploratória dos valores de carros dos USA", text_alignment = 'center' )
 
-question_box = st.multiselect('Gostaria de analiser estatisticas de carro?', ["sim", "não"])
+question_box = st.radio("Gostaria de analisar estatísticas de carro?", ["sim", "não"])
 
 if "sim" in question_box:
     #Dando as escolhas possiveis
-    choices = st.multiselect("Quais gráficos quer ver?", ["hist", "scatter", "both"])
+    choices = st.radio("Quais gráficos quer ver?", ["hist", "scatter", "both"])
 
     #SE escolher his, plora um hist, e se escolher both tabm
     if "hist" in choices or "both" in choices:
@@ -25,7 +25,7 @@ if "sim" in question_box:
         st.plotly_chart(fig1, use_container_width=True)
 
 if "não" in question_box:
-    print("Obrigado pela visita, tenha um bom dia")
+    st.write("Obrigado pela visita, tenha um bom dia")
 
 
     
