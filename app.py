304602a46c4ec04e40c6bc5ccad340391dca0a33
LianChristofer <liancsbertuola@gmail.com>
2026-03-19 16:09:06 +0000

    mod1

--- app.py
+++ app.py
@@ -6,16 +6,47 @@ car_data = pd.read_csv('vehicles.csv')
 
 st.header("Analise Exploratória dos valores de carros dos USA", text_alignment = 'center' )
 
-hist_button = st.button('Criar um Histogram?')
+hist_button = st.button('Gostaria de analiser estatisticas de carro?')
 
 if hist_button:
 
-    #Criando uma msg
-    st.write("Criando um Histograma...")
+    question_box = st.checkbox(["his", "scatter", "both"])
 
-    #Criando um Histograma
-    fig = px.histogram(car_data, x='odometer', nbins=100)
+    if "his" in question_box:
+        #Criando uma msg
+        st.write("Criando um Histograma...")
 
-    #Plotando o histograma
-    st.plotly_chart(fig, use_container_width=True)
+        #Criando um Histograma
+        fig = px.histogram(car_data, x='odometer', y="price", nbins=100)
+
+        #Plotando o histograma
+        st.plotly_chart(fig, use_container_width=True)
+
+    if "scatter" in question_box:
+        #Criando uma msg
+        st.write("Criando um grafico de dispeção...")
+
+        #Criando um Histograma
+        fig1 = px.scatter(car_data, x='odometer', y="price")
+
+        #Plotando o histograma
+        st.plotly_chart(fig1, use_container_width=True)
+
+    if "both" in question_box:
+        #Criando uma msg
+        st.write("Criando os gráficos...")
+
+        #Criando um Histograma
+        fig = px.histogram(car_data, x='odometer', y="price", nbins=100)
+
+        #Plotando o histograma
+        st.plotly_chart(fig, use_container_width=True)
+
+        #Criando um grafico de disperção
+        fig1 = px.scatter(car_data, x='odometer', y="price")
+
+        #Plotando o grafico
+        st.plotly_chart(fig1, use_container_width=True)
+
+    
 
